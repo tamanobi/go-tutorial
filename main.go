6c5af0721c56a2405a320abf01666c846de15592
tamanobi <tamanobi@gmail.com>
2019-10-10 19:00:52 +0900

    答えを隠した

--- main.go
+++ main.go
@@ -63,66 +63,6 @@ func main() {
 		w.Write(m) // JSONをサーバーから返す
 	})
 
-	http.HandleFunc("/simple2", func(w http.ResponseWriter, req *http.Request) {
-		// レスポンスがJSONであると、クライアントへ教える
-		w.Header().Set("Content-Type", "application/json")
-		// 空の構造体(struct{}{})をJSONに変換する
-		m, err := json.Marshal(struct {
-			Name string `json:"name"`
-		}{
-			"太郎",
-		})
-		// 変換処理のエラーチェック
-		if err != nil {
-			// エラーだったら、InternalServerErrorを返す
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(m) // JSONをサーバーから返す
-	})
-
-	http.HandleFunc("/simple3", func(w http.ResponseWriter, req *http.Request) {
-		// レスポンスがJSONであると、クライアントへ教える
-		w.Header().Set("Content-Type", "application/json")
-		m, err := json.Marshal(struct {
-			Error bool `json:"has_error"` // 成功か失敗を表す
-			Body  int  `json:"body"`      // 値
-		}{
-			true, calc.Abs(-10),
-		})
-		// 変換処理のエラーチェック
-		if err != nil {
-			// エラーだったら、InternalServerErrorを返す
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(m) // JSONをサーバーから返す
-	})
-
-	http.HandleFunc("/simple4", func(w http.ResponseWriter, req *http.Request) {
-		// レスポンスがJSONであると、クライアントへ教える
-		w.Header().Set("Content-Type", "application/json")
-		out, err := calc.Div(1, 0)
-		if err != nil {
-			// エラーだったら、InternalServerErrorを返す
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		m, err := json.Marshal(struct {
-			Error bool    `json:"has_error"` // 成功か失敗を表す
-			Body  float64 `json:"body"`      // 値
-		}{
-			true, out,
-		})
-		// 変換処理のエラーチェック
-		if err != nil {
-			// エラーだったら、InternalServerErrorを返す
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(m) // JSONをサーバーから返す
-	})
 	// サーバーを実行する
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
